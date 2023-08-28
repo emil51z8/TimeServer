3401ba07071725882ea4333025b3d6f9b96f3d3c
emil51z8 <emil51z8@edu.zealand.dk>
2023-08-28 15:11:25 +0200

    Ændringer lavet. Nu fungere det automatisk og uden at man skal indtaste noget for at se tiden.

--- src/Client.java
+++ src/Client.java
@@ -9,7 +9,6 @@ public class Client {
     static int port = 2001;
     static String host = "localhost";
     static DataInputStream in;
-    static DataOutputStream out;
     static Socket socket;
 
     public static void main(String[] args) {
@@ -17,24 +16,12 @@ public class Client {
         try {
             socket = new Socket(host, port);
             in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        while(true) {
-            System.out.println("Indtast 1 hvis du vil se klokken: ");
-            Scanner input = new Scanner(System.in);
-
-
             try {
-                String send = input.next();
-
-
-                out.writeUTF(send);
                 System.out.println(in.readUTF());
-
-
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -43,4 +30,3 @@ public class Client {
 
 
 
-}
--- src/Server.java
+++ src/Server.java
@@ -7,7 +7,6 @@ import java.util.Date;
 
 public class Server {
     static int port = 2001;
-    static DataInputStream in;
     static DataOutputStream out;
     static ServerSocket server;
     static Socket socket;
@@ -16,27 +15,21 @@ public class Server {
         try {
             server = new ServerSocket(port);
             socket = server.accept();
-            in = new DataInputStream(socket.getInputStream());
+
             out = new DataOutputStream(socket.getOutputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        while (true) {
-            try {
 
-                String modtaget = in.readUTF();
-                if(modtaget.equals("1")){
-                    System.out.println("Besked modtaget: " + modtaget);
-                    out.writeUTF("Klokken er : " + new Date().toString());
-                }
-
-
-
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            out.writeUTF("Den aktuelle tid er : " + new Date().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
 }
+
+
